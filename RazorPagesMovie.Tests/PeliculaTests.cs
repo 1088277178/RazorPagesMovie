67975c3372d6cfd6ca22fead878f8ed9a6a3dc8a
USER <USER@DESKTOP-M6L51BK>
2025-02-16 22:00:14 -0500

    Agregar archivos de proyecto.

---
+++ RazorPagesMovie.Tests/PeliculaTests.cs
@@ -0,0 +1,28 @@
+﻿
+using RazorPagesMovie.Models;
+using Xunit;
+
+namespace RazorPagesMovie.Tests
+{
+    public class PeliculaTests
+    {
+        [Fact]
+        public void TestMovieTitle()
+        {
+            // objeto con valores
+            var movie = new Movie
+            {
+                Title = "Rambo",
+                ReleaseDate = DateTime.Parse("1990-03-17"),
+                Genre = "Action",
+                Price = 9.99M
+            };
+
+            // valores de la propiedad title
+            var title = movie.Title;
+
+            // validación valor title sea "Rambo"
+            Assert.Equal("Rambo", title);
+        }
+    }
+}
